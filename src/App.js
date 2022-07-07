75e0efe13c568730629459554921243bfdadf8f9
sirben181 <bensonkamauben@gmail.com>
2022-07-07 12:16:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Routes,Route} from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Navbar/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
