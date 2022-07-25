bf01b16496dfe51345934f2ad82497f254d5ff1d
sirben181 <bensonkamauben@gmail.com>
2022-07-25 17:17:45 +0300

    body content started

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,21 @@ const Home = () => {
   return (
     <div className='home-container'>
       <h1>Home</h1>
-      <p>this is the home page </p>
+      <p>this is the home page 
+        <div className="home-wrapper">
+          <div className="home">
+
+          </div>
+        </div>
     </div>
   )
 }
-
+// qualities of a good home page
+// visible and a clear navigation
+// it should show the main service that you are offering probably with some visuals
+// it should alaso include the benefits of these services that you are offering.
+// strong hero section and testimonials probably with the images and messages of these guys.
+// have your primary call to action- what is it that you want your viewers to do after visiting you site-should be easy  to do.
+// it should be unique differently styled and strategically placed
+// the secondry call to action is to add them to the email list-- so have them to give them a name,email and maybe phone number
 export default Home
