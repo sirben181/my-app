26d310ae1e7fe08e23eebd96cde725585e040c1a
sirben181 <bensonkamauben@gmail.com>
2022-07-26 20:37:08 +0300

    content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './home.css'
+import home from '../images/bg-pic.jpg'
 const Home = () => {
   return (
     <div className='home-container'>
          <div className="home-wrapper">
             <div className="home">
             <h2>Home</h2>
-              <img src="" alt="" />
+              <img src={home} alt="swafi"/>
+            </div>
+            <div className="projects">
+              <h3>welcome <strong>Here</strong> are some of our projects that we have done so far</h3>
+              <div className="project-card">
+                hello this is the project
+              </div>
             </div>
          </div>
     </div>
