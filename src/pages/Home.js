1ad5dfc2769c3c24a92a129f45266407773c0db8
sirben181 <bensonkamauben@gmail.com>
2022-08-04 12:09:10 +0300

    more on projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,42 +2,103 @@ import React from 'react'
 import './home.css'
 import home from '../images/bg-pic.jpg'
 import Profile from './Profile'
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className='home-container'>
          <div className="home-wrapper">
-            <div className="home">
-              <img src={home} alt="swafi"/>
-              <p>welcome and lets <br/>learn web development together</p>
-            </div>
-            <Profile />
-            <div className="projects">
-              <h3>welcome to Our Projects</h3>
-              <div className="cards">
-                <div className="project-card">
-                  hello this is the project
-                </div>
-                <div className="project-card">
-                  hello this is the project
-                </div>
+              <div className="home">
+                <img src={home} alt="swafi"/>
+                <p>welcome and lets <br/>learn web development together</p>
               </div>
-              <div className="cards">
-                <div className="project-card">
-                  hello this is the project
-                </div>
-                <div className="project-card">
-                  hello this is the project
-                </div>
-              </div>
-              <div className="cards">
-                <div className="project-card">
-                  hello this is the project
-                </div>
-                <div className="project-card">
-                  hello this is the project
-                </div>
-              </div>
-            </div>
+              <Profile />
+              <div className="projects">
+                  <h3>welcome to Our Projects</h3>
+                  <div className="cards">
+                        <div className="project-card">
+                            <div className="project-card-header">
+                                <h3> hello this is the project </h3>
+                              </div>
+                              <div className="card-body">
+                                <img src="" alt="image is here" />
+                                  <Link to='/projects/project-1'>
+                                <button className='btn-primary'>
+                                    Find out more
+                                  </button>
+                                  </Link> 
+                            </div>
+                        </div>
+                        <div className="project-card">
+                            <div className="project-card-header">
+                                <h3> hello this is the project </h3>
+                              </div>
+                              <div className="card-body">
+                                <img src="" alt="image is here" />
+                                  <Link to='/projects/project-2'>
+                                <button className='btn-primary'>
+                                    Find out more
+                                  </button>
+                                  </Link> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cards">
+                        <div className="project-card">
+                            <div className="project-card-header">
+                                <h3> hello this is the project </h3>
+                              </div>
+                              <div className="card-body">
+                                <img src="" alt="image is here" />
+                                  <Link to='/projects/project-3'>
+                                <button className='btn-primary'>
+                                    Find out more
+                                  </button>
+                                  </Link> 
+                            </div>
+                        </div>
+                        <div className="project-card">
+                            <div className="project-card-header">
+                                <h3> hello this is the project </h3>
+                              </div>
+                              <div className="card-body">
+                                <img src="" alt="image is here" />
+                                  <Link to='/projects/project-4'>
+                                <button className='btn-primary'>
+                                    Find out more
+                                  </button>
+                                  </Link> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cards">
+                        <div className="project-card">
+                            <div className="project-card-header">
+                                <h3> hello this is the project </h3>
+                              </div>
+                              <div className="card-body">
+                                <img src="" alt="image is here" />
+                                  <Link to='/projects'>
+                                <button className='btn-primary'>
+                                    Find out more
+                                  </button>
+                                  </Link> 
+                            </div>
+                        </div>
+                        <div className="project-card">
+                            <div className="project-card-header">
+                                <h3> hello this is the project </h3>
+                              </div>
+                              <div className="card-body">
+                                <img src="" alt="image is here" />
+                                  <Link to='/projects'>
+                                <button className='btn-primary'>
+                                    Find out more
+                                  </button>
+                                  </Link> 
+                            </div>
+                        </div>
+                    </div>
+               </div>
          </div>
     </div>
   )
