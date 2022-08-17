b44403454c1e03a4e3996e1f0a55dc7de7caa7ea
sirben181 <bensonkamauben@gmail.com>
2022-08-17 18:16:36 +0300

    starting on the blog designs

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,8 +4,19 @@ const Blog = () => {
   return (
     <div className='blog-container'>
       <div className="blog-wrapper">
-      <h1>Blog</h1>
-
+        <div className="blog">
+          <div className="blog-header">
+           <h2>blog</h2>
+          </div>
+          <div className="blog-body">
+            <div className="blog-content"></div>
+            <div className="blog-footer">
+              <div className="like"><p>Like</p></div>
+              <div className="comment"><p>Like</p></div>
+              <div className="like"><p>Like</p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
