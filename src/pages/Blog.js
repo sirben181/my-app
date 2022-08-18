b46e4336b273fe5bc051f0dae314fa2b9ec0eddd
sirben181 <bensonkamauben@gmail.com>
2022-08-18 14:14:04 +0300

    over there

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -6,14 +6,19 @@ const Blog = () => {
       <div className="blog-wrapper">
         <div className="blog">
           <div className="blog-header">
-           <h2>blog</h2>
+            <h2>How to create a length converter</h2>
           </div>
           <div className="blog-body">
-            <div className="blog-content"></div>
-            <div className="blog-footer">
+            <div className="blog-content">
+              <p>This will require three files .html, .css, .js files <br /> 
+              that are linked together. Also you can the inline and internal <br />
+               styling can be done and also js files can be written. <br />
+               This is done using only js and it looks very cool</p>
+            </div>
+            <div className="blog-footer"> 
               <div className="like"><p>Like</p></div>
-              <div className="comment"><p>Like</p></div>
-              <div className="like"><p>Like</p></div>
+              <div className="comment"><p>comment</p></div>
+              <div className="share"><p>share</p></div>
             </div>
           </div>
         </div>
