1373248c479a0cf7d19df18e53daff3ea1bd188e
sirben <bensonkamauben@gmail.com>
2025-03-08 19:44:05 +0300

    about design

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,30 +4,31 @@ import Profile from './Profile'
 const About = () => {
   return (
     <div className='about-container'>
-      <div className="about-wrapper">
-        <h1>About</h1>
-         <div className='about'>
-            <div className="about-header">
-               <div className='about-header'>
-                    <Profile/>
-              </div>
-            </div>
-            <div className='about-body'>
-               <div className='about-body-wrapper'>
-                   <p> this is the best website for web devlopment stories and tips.
-                   This is just amazing and lets be okay with it.
-                   </p>
-                   <p>
-                        This has been inspired by paul
-                  </p>
-               </div>
+        <div className="about-wrapper">
+              <h1>About</h1>
+              <div className='about'>
+                          <div className='about-header'>
+                            <div className='about-header-wrapper'>
+                                <Profile/>
+                            </div>
+                          </div>
+                          <div className='about-body'>
+                                <div className='about-body-wrapper'>
+                                        <p> this is the best website for web devlopment stories and tips.
+                                        This is just amazing and lets be okay with it.
+                                        </p>
+                                        <p>
+                                              This has been inspired by paul
+                                        </p>
+                                </div>
+                          </div>
             </div>
          </div>
       
 
       </div>
   
-    </div>
+    
   )
 }
 
