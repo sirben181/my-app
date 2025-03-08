5f6fe43096fd791b10ac050fbd02e11fece0e127
sirben <bensonkamauben@gmail.com>
2025-03-08 07:45:34 +0300

    working on about

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,14 +6,18 @@ const About = () => {
     <div className='about-container'>
       <div className="about-wrapper">
         <h1>About</h1>
-         <div className='about-body'>
+         <div className='about'>
+            <div className="about-header">
           <Profile/>
+            </div>
+            <div className='about'>
             <p> this is the best website for web devlopment stories and tips.
               This is just amazing and lets be okay with it.
             </p>
             <p>
               This has been inspired by paul
             </p>
+            </div>
          </div>
       
 
